1d003eb1e1a712eebdf4bb35f2a17d46a86af3f7
sukhijaa <abhisukhija@ymail.com>
2020-04-18 01:43:53 +0530

    Styling fix

--- src/screens/Home/styles.js
+++ src/screens/Home/styles.js
@@ -1,10 +1,14 @@
 import { StyleSheet } from 'react-native';
+import {
+  responsiveFontSize,
+  responsiveScreenWidth,
+} from 'react-native-responsive-dimensions';
 
 const styles = StyleSheet.create({
   container: {
     alignItems: 'center',
     backgroundColor: 'black',
-    height: '100%'
+    height: '100%',
   },
   text: {
     color: 'white',
@@ -17,16 +21,17 @@ const styles = StyleSheet.create({
     marginTop: 50,
   },
   startGameButton: {
-    width: 200,
-    height: 100,
-    backgroundColor: 'orange',
+    height: 'auto',
+    width: responsiveScreenWidth(100) - 40,
+    marginHorizontal: 20,
+    marginVertical: 10,
   },
   startGameButtonLabel: {
-    fontSize: 20,
+    fontSize: responsiveFontSize(2),
   },
   textInput: {
     margin: 20,
-    width: '60%'
+    width: '60%',
   },
 });
 
